cae6de4c73273a212decd992e6beca16ebc544dc
Abhishek Yadav <abhishek26321@gmail.com>
2024-08-28 23:04:32 +0530

    Added user methods

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,13 +1,26 @@
 package controllers
 
 import (
+<<<<<<< HEAD
 	helper "go-auth-jwt/helpers"
 	"net/http"
 	"next/http"
 
 	"github.com/abh1shekyadav/go-auth-jwt/database"
+=======
+	"context"
+	helper "go-auth-jwt/helpers"
+	"log"
+	"net/http"
+	"next/http"
+	"time"
+
+	"github.com/abh1shekyadav/go-auth-jwt/database"
+	"github.com/abh1shekyadav/go-auth-jwt/models"
+>>>>>>> f3e3c46 (Added user methods)
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -18,17 +31,67 @@ func HashPassword()
 
 func VerifyPassword()
 
-func SignUp()
+func SignUp() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		validationErr := validate.Struct(user)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+		count, err := userCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occured while checking for the email"})
+		}
+
+		count, err := userCollection.CountDocuments(ctx, bson.M{"phone": user.Phone})
+		defer cancel()
+		if err != nil {
+			log.Panic(err)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error occured while checking for the phone number"})
+		}
+
+		if count > 0 {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Email or phone number already exists"})
+		}
+
+	}
+}
 
 func Login()
 
 func GetUsers()
 
 func GetUser() gin.HandlerFunc {
+<<<<<<< HEAD
 	return func(ctx *gin.Context) {
 		userId := ctx.Param("user_id")
 		if err := helper.MatchUserTypeToUid(ctx, userId); err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		}
+=======
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
+		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+>>>>>>> f3e3c46 (Added user methods)
 	}
 }
