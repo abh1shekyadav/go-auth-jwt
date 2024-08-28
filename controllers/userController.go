c53b12aff3d81a0957336f5eb2784b5ce7cb6837
Abhishek Yadav <abhishek26321@gmail.com>
2024-08-28 23:04:32 +0530

    Added user methods

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,7 +1,12 @@
 package controllers
 
 import (
+	helper "go-auth-jwt/helpers"
+	"net/http"
+	"next/http"
+
 	"github.com/abh1shekyadav/go-auth-jwt/database"
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -19,4 +24,11 @@ func Login()
 
 func GetUsers()
 
-func GetUser()
+func GetUser() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		userId := ctx.Param("user_id")
+		if err := helper.MatchUserTypeToUid(ctx, userId); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+	}
+}
